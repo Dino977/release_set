143a961819b4dee6e7d9a77d6694ad461eecb5df
zhoupeiqian <zhoupeiqian@seewintech.com>
2024-07-19 17:49:45 +0800

    pref: 新增 scss文件自动创建 功能（由components.json与gen命令配合完成）

---
+++ x-ui/build/bin/gen-cssfile.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+
+const components = require("../../components.json"); // 组件配置文件
+const componentNames = Object.keys(components); // 组件名称集合
+// 定位目标文件夹的绝对路径
+const basePath = path.resolve(__dirname, "../../packages/");
+// 判断文件是否存在的方法
+function fileExists(fullPath) {
+  try {
+    return fs.statSync(fullPath).isFile();
+  } catch (err) {
+    return false;
+  }
+}
+
+componentNames.forEach((name) => {
+  // 下述组件，不单独创建样式文件
+  if (['icon', 'option', 'option-group'].includes(name)) return
+
+  // 由组件名，构建样式文件名称（如：input -> input.scss）
+  const fileName = `${name}.scss`;
+  // 构建样式文件的全路径
+  const fullPath = path.resolve(basePath, 'theme-chalk/src', fileName);
+  // 判断该样式文件是否存在
+  if (!fileExists(fullPath)) {
+    // 仅当未存在时，构建空文件
+    fs.writeFileSync(fullPath, '', 'utf-8');
+    console.log('新创建文件：', fileName)
+  }
+});
